40e4ab902c9f020c1165620f4d9cd4aa54dfdc58
jchris <jchris1227@icloud.com>
2022-10-08 23:20:33 -0700

    reset from main

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
 #Write your code below this row 👇
 #Testing GIT REMOTE
-print('this is from the main branch')
-print('changes from main')
-print('changes from test1 branch')
-print('changes from vm branch')
